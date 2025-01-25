66496f4fafcac1fa2aef2796db5f1eb82b819bbe
Bruno <bruno.silva.castilho@gmail.com>
2025-01-25 20:40:50 -0300

    Estrutura visual da home

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: [
+    'page.tsx',
+    'api.ts',
+    'api.tsx'
+  ]
 };
 
 export default nextConfig;
