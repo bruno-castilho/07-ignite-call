f725e271471234ab19cbad5a3ecec12580e89753
Bruno <bruno.silva.castilho@gmail.com>
2025-02-02 16:14:49 -0300

    Navegando nos meses

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,21 +8,43 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from './styles'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth(){
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth(){
+    const previousMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          <span></span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button title="Previous month">
+          <button title="Previous month" onClick={handlePreviousMonth}>
             <CaretLeft />
           </button>
-          <button title="Next month">
+          <button title="Next month" onClick={handleNextMonth}>
             <CaretRight />
           </button>
         </CalendarActions>
