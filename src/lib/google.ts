7081efdd23ff0389ffecc27011f85315d581e558
Bruno <bruno.silva.castilho@gmail.com>
2025-02-04 20:09:24 -0300

    Criação do evento no Google

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -1,7 +1,6 @@
+import dayjs from 'dayjs'
 import { google } from 'googleapis'
 import { prisma } from './prisma'
-import dayjs from 'dayjs'
-
 export async function getGoogleOAuthToken(userId: string) {
   const account = await prisma.account.findFirstOrThrow({
     where: {
@@ -9,22 +8,19 @@ export async function getGoogleOAuthToken(userId: string) {
       user_id: userId,
     },
   })
-
   const auth = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
   )
-
   auth.setCredentials({
     access_token: account.access_token,
     refresh_token: account.refresh_token,
     expiry_date: account.expires_at ? account.expires_at * 1000 : null,
   })
-
-  if (!account.expires_at) return auth
-
+  if (!account.expires_at) {
+    return auth
+  }
   const isTokenExpired = dayjs(account.expires_at * 1000).isBefore(new Date())
-
   if (isTokenExpired) {
     const { credentials } = await auth.refreshAccessToken()
     const {
@@ -35,7 +31,6 @@ export async function getGoogleOAuthToken(userId: string) {
       scope,
       token_type,
     } = credentials
-
     await prisma.account.update({
       where: {
         id: account.id,
@@ -49,13 +44,11 @@ export async function getGoogleOAuthToken(userId: string) {
         token_type,
       },
     })
-
     auth.setCredentials({
       access_token,
       refresh_token,
       expiry_date,
     })
-
-    return auth
   }
+  return auth
 }
